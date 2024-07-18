cf82a866f0442985d1b107232a22692d5ccf9e30
Elisabeth Martinez <emarti17@artic.edu>
2024-07-18 13:24:55 -0700

    added custom favicon and edited import syntax on pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
-import './page.css'
+import "./page.css"
 import IntroSection from "./components/IntroSection";
 import ResumeBar from "./components/ResumeBar";
 import Projects from "./components/Projects";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className='root'>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <IntroSection/>
       <div className='main-section'>
         <ResumeBar/>
