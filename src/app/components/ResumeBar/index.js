b5cb0c1f1b6339b11706f5674b5c6b51f8a6d082
Elisabeth Martinez <emarti17@artic.edu>
2024-09-16 09:31:49 -0700

    updated resue link

--- src/app/components/ResumeBar/index.js
+++ src/app/components/ResumeBar/index.js
@@ -4,7 +4,7 @@ const ResumeBar = () => {
   return(
     <div className='resume-bar'>
     <a 
-      href="https://docs.google.com/document/d/1mVpnXd2tkyfJHopa8UBYwfDficBOGIXEfNX7tHAauDQ/edit?usp=sharing" 
+      href="https://docs.google.com/document/d/1VoFZIzcLPKyZQzONgfvdmAV5-c_Xel2wsFYEoKhsxyI/edit?usp=sharing" 
       target="_blank"
       download
       className='resume-doc-link'
